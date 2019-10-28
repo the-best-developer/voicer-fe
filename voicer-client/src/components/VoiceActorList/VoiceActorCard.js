8a702372d3804814f47f8db98172dda0d63c6a30
the-best-developer <im.dylan.gosnell@gmail.com>
2019-10-28 18:09:57 -0400

    initial voice actor list feature commit

---
+++ voicer-client/src/components/VoiceActorList/VoiceActorCard.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Card, Button, CardTitle, CardText, CardSubtitle, CardBody } from 'reactstrap';
+import styled from 'styled-components'
+
+// Styling
+const StyledCard = styled(Card)`
+  width: 31%;
+  border: 1px solid red;
+  margin: 10px 0;
+  min-height: 100px;
+`
+
+const StyledButton = styled(Button)`
+  margin: 0 10px;
+`
+
+const StyledCardBody = styled(CardBody)`
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+`
+
+const StyledButtonDiv = styled.div`
+  border: 1px solid red;
+  padding: 10px;
+  margin-left: auto;
+`
+
+// Component
+const VoiceActorCard= props => {
+
+    return (
+        <StyledCard>
+            <StyledCardBody>
+                <CardTitle>{props.vaData.firstName} {props.vaData.lastName}</CardTitle>
+                <CardSubtitle>{props.vaData.userType}</CardSubtitle>
+                <CardText>{props.vaData.email}</CardText>
+                <StyledButtonDiv>
+                    <StyledButton>Click me!</StyledButton>
+                    <StyledButton>Or me!</StyledButton>
+                </StyledButtonDiv>
+            </StyledCardBody>
+        </StyledCard>
+    );
+};
+
+export default VoiceActorCard;
