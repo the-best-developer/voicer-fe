ecbefc905d68e6172b91a3ecff5cc32d117e3c9e
Ken Ruf <ruf.kenichi@gmail.com>
2019-10-29 22:18:50 -0500

    fixing auth endpoint URLs

--- voicer-client/src/actions/register.js
+++ voicer-client/src/actions/register.js
@@ -7,7 +7,7 @@ export const REGISTER_FAILED = 'REGISTER-FAILED';
 export const register = creds => dispatch => {
     dispatch({ type: REGISTER_START });
     return axios
-        .post('https://voicer-lambda-app-staging.herokuapp.com/api/register', creds) // CHANGE URL
+        .post('https://voicer-lambda-app-staging.herokuapp.com/api/auth/register', creds) // CHANGE URL
         .then(res => {
             localStorage.setItem('token', res.data.token)
             dispatch({
