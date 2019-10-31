97caab390602680ffa684769a1260947fb311284
Ken Ruf <ruf.kenichi@gmail.com>
2019-10-30 22:42:30 -0500

    switched getJobs endpoint URL to Heroku Staging App

--- voicer-client/src/actions/getJobs.js
+++ voicer-client/src/actions/getJobs.js
@@ -7,8 +7,8 @@ export const GET_JOBS_FAILED = 'GET_JOBS-FAILED';
 export const getJobs = () => dispatch => {
     dispatch({ type: GET_JOBS_START });
     return axios
-        //.get('https://voicer-lambda-app-staging.herokuapp.com/api/jobs')
-        .get('http://localhost:4000/api/jobs')
+        .get('https://voicer-lambda-app-staging.herokuapp.com/api/jobs')
+        //.get('http://localhost:4000/api/jobs')
         .then(res => {
             dispatch({
                 type: GET_JOBS_SUCCESS,
