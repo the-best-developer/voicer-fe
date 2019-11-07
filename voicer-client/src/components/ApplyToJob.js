9b4e2b760eab2520d34f25a3092cb16e8dbb807e
Ken Ruf <ruf.kenichi@gmail.com>
2019-11-06 23:01:01 -0600

    added Modal

--- voicer-client/src/components/ApplyToJob.js
+++ voicer-client/src/components/ApplyToJob.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {Form, FormGroup, Label, Input, Button} from 'reactstrap';
+import {Form, FormGroup, Label, Input, Button, Modal, ModalHeader} from 'reactstrap';
 
 class ApplyToJob extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-
+            isOpen: false,
         }
     }
 
@@ -17,17 +17,23 @@ class ApplyToJob extends React.Component {
 
     }
 
+    toggle = () => this.setState({isOpen: !this.state.isOpen})
+
     render() {
         return(
-            <div className="JobDetails">
-
-            </div>
-            <Form className="ApplyToJob">
-                <FormGroup>
-                    <Label></Label>
-                    <Input></Input>
-                </FormGroup>
-            </Form>
+            <Modal
+                isOpen={this.state.isOpen}
+                toggle={toggle}
+                centered={true}
+            >
+                <ModalHeader>{this.props.jobTitle}</ModalHeader>
+                <Form className="ApplyToJob">
+                    <FormGroup>
+                        <Label></Label>
+                        <Input></Input>
+                    </FormGroup>
+                </Form>
+            </Modal>
         )
     }
 }
