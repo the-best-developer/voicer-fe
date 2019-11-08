d90caf714e849d8d22ed69514717e6132f39d001
Ken Ruf <ruf.kenichi@gmail.com>
2019-11-07 22:44:31 -0600

    Basic Styling

--- voicer-client/src/components/ApplyToJob.js
+++ voicer-client/src/components/ApplyToJob.js
@@ -1,5 +1,35 @@
 import React from 'react';
 import {Form, FormGroup, Label, Input, Button, Modal, ModalHeader} from 'reactstrap';
+import styled from 'styled-components';
+
+//Styling
+const StyledModal = styled(Modal)`
+    padding: 25px;
+    width: 50%;
+`
+
+const StyledModalHeader = styled(ModalHeader)`
+    margin: 25px;
+`
+
+const StyledP = styled.p`
+    margin: 25px;
+`
+
+const StyledForm = styled(Form)`
+    margin: 25px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const StyledButton = styled(Button)`
+    width: 50%;
+    margin: 0 auto;
+`
+const StyledFormGroup = styled(FormGroup)`
+    width: 100%;
+`
 
 class ApplyToJob extends React.Component {
     constructor(props) {
@@ -16,20 +46,22 @@ class ApplyToJob extends React.Component {
 
     render() {
         return(
-            <Modal
+            <StyledModal
                 isOpen={this.props.isOpen}
                 toggle={this.props.toggle}
                 centered={true}
+                size="lg"
             >
-                <p>You are Applying to:</p>
-                <ModalHeader>{this.props.job.jobTitle}</ModalHeader>
-                <Form className="ApplyToJob">
-                    <FormGroup>
-                        <Label>Write a Message to the Client</Label>
+                <StyledP>You are Applying to:</StyledP>
+                <StyledModalHeader>{this.props.job.jobTitle}</StyledModalHeader>
+                <StyledForm className="ApplyToJob">
+                    <StyledFormGroup>
+                        <Label>Leave the Client a Message</Label>
                         <Input type="textarea"></Input>
-                    </FormGroup>
-                </Form>
-            </Modal>
+                    </StyledFormGroup>
+                    <StyledButton>Apply</StyledButton>
+                </StyledForm>
+            </StyledModal>
         )
     }
 }
