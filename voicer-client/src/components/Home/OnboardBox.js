5cd6fc22fe18d4d33be9c7080619c23b4aa0505c
Cesare Ferrari <cesare@maryandferrari.com>
2019-11-09 14:10:04 -0500

    Style hero

--- voicer-client/src/components/Home/OnboardBox.js
+++ voicer-client/src/components/Home/OnboardBox.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Jumbotron } from 'reactstrap'
+import { Jumbotron } from 'reactstrap';
+import OnboardCard from './OnboardCard';
 // import './onboardBox.css'
 
 const OnboardBox = () => {
     return (
         <div className="hero-box">
             <Jumbotron className="hero">
-                <h1 className="display-3 header">Get Started!</h1>
-                <div className="choice">
-                    <Link className="buttons lead" to="/register">REGISTER</Link>
-                    <Link className="buttons lead" to="/login">LOGIN</Link>
-                </div>
+              <OnboardCard />
             </Jumbotron>
         </div>
     )
