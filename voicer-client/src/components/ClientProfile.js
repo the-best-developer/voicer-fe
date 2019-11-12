36937d52156a8077365fb3aeb43b765e9d9395e5
Christian MacDonald <39752557+ChristianMacDonald@users.noreply.github.com>
2019-11-11 23:06:52 -0500

    Styled the ClientProfile component

--- voicer-client/src/components/ClientProfile.js
+++ voicer-client/src/components/ClientProfile.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { getClientProfile } from '../actions';
 import { Container, Row, Col, Card, CardBody, CardTitle, CardText, FormGroup, Label, Input } from 'reactstrap';
+import ClientNavigation from './ClientNavigation'
+import './ClientProfile.scss';
 
 class ClientProfile extends React.Component {
     constructor(props) {
@@ -14,32 +16,35 @@ class ClientProfile extends React.Component {
     
     render() {
         return (
-            <Container>
-                <Row>
-                    <Col>
-                        <Card>
-                            <CardBody>
-                                <CardTitle><h1>Profile</h1></CardTitle>
-                            </CardBody>
-                            <CardBody>
-                                <CardText>{this.props.clientProfile ? this.props.clientProfile.email : 'You are not signed in.'}</CardText>
-                            </CardBody>
-                            <CardBody>
-                                <FormGroup>
-                                    <Label for="username">Username</Label>
-                                    <Input disabled id="username" value={this.props.clientProfile ? this.props.clientProfile.username : 'You are not signed in.'} />
-                                    <Label for="first_name">First Name</Label>
-                                    <Input disabled id="first_name" value={this.props.clientProfile ? this.props.clientProfile.firstName : 'You are not signed in.'} />
-                                    <Label for="last_name">Last Name</Label>
-                                    <Input disabled id="last_name" value={this.props.clientProfile ? this.props.clientProfile.lastName : 'You are not signed in.'} />
-                                    <Label for="company_name">Company Name</Label>
-                                    <Input disabled id="company_name" value={this.props.clientProfile ? this.props.clientProfile.companyName : 'You are not signed in.'} />
-                                </FormGroup>
-                            </CardBody>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
+            <div>
+                <ClientNavigation />
+                <Container>
+                    <Row>
+                        <Col>
+                            <Card>
+                                <CardBody>
+                                    <CardTitle><h1>Profile</h1></CardTitle>
+                                </CardBody>
+                                <CardBody>
+                                    <CardText>{this.props.clientProfile ? this.props.clientProfile.email : 'You are not signed in.'}</CardText>
+                                </CardBody>
+                                <CardBody>
+                                    <FormGroup>
+                                        <Label for="username">Username</Label>
+                                        <Input disabled id="username" value={this.props.clientProfile ? this.props.clientProfile.username : 'You are not signed in.'} />
+                                        <Label for="first_name">First Name</Label>
+                                        <Input disabled id="first_name" value={this.props.clientProfile ? this.props.clientProfile.firstName : 'You are not signed in.'} />
+                                        <Label for="last_name">Last Name</Label>
+                                        <Input disabled id="last_name" value={this.props.clientProfile ? this.props.clientProfile.lastName : 'You are not signed in.'} />
+                                        <Label for="company_name">Company Name</Label>
+                                        <Input disabled id="company_name" value={this.props.clientProfile ? this.props.clientProfile.companyName : 'You are not signed in.'} />
+                                    </FormGroup>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         );
     }
 }
