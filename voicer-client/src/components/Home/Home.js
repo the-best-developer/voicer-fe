db7aa6c7066600eea4dccc914815da6778167780
Cesare Ferrari <cesare@maryandferrari.com>
2019-11-11 23:46:44 -0500

    Add footer to all pages

--- voicer-client/src/components/Home/Home.js
+++ voicer-client/src/components/Home/Home.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import NavBar from './NavBar'
 import InfoBox from './InfoBox';
-import Footer from '../Footer';
 import OnboardBox from './OnboardBox';
 
 const Home = () => {
     return (
         <div className='home'>
-
             <NavBar />
             <OnboardBox />
             <InfoBox />
-
-            <Footer />
         </div>
     )
 }
