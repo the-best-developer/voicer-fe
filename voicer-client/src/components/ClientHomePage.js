4e464764aaff45ef1fac9f01cbe8d856381399c1
Ken Ruf <ruf.kenichi@gmail.com>
2019-11-13 22:19:34 -0600

    get jobs by clientId

--- voicer-client/src/components/ClientHomePage.js
+++ voicer-client/src/components/ClientHomePage.js
@@ -4,6 +4,8 @@ import { getJobsBy } from '../actions';
 import jwt from 'jsonwebtoken';
 import ClientJobList from './JobList/ClientJobList';
 import axiosWithAuth from './axiosAuth';
+import { Button } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 class ClientHomePage extends React.Component {
     constructor(props) {
@@ -14,13 +16,16 @@ class ClientHomePage extends React.Component {
     }
 
     componentDidMount = async () => {
-        const client = await axiosWithAuth().get(`https://voicer-lambda-app-staging.herokuapp.com/api/clients/${this.state.userId}`)
+        const client = await axiosWithAuth().get(`https://voicer-lambda-app-staging.herokuapp.com/api/clients/${this.state.userId}/jobs`)
         this.props.getJobsBy(client.data[0].clientId)
     }
 
     render() {
         return(
-            <ClientJobList jobs={[this.props.jobs]} />
+            <div className="ClientHomePage">
+                <ClientJobList jobs={[...this.props.jobs]} />
+                <Link to="/client/postJob"><Button>Post Job</Button></Link>
+            </div>
         )
     }
 }
