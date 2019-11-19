54ce66c948911ca348bf1ac2f3a9b9b329bcca4f
Jeremy Morales <jeremy.morales@gmail.com>
2019-11-19 15:26:41 -0800

    language/accents for talentprofile from redux

--- voicer-client/src/components/TalentProfile.js
+++ voicer-client/src/components/TalentProfile.js
@@ -1,147 +1,189 @@
 import React from 'react';
 import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
 import Select, { components } from 'react-select';
+import { connect } from 'react-redux';
+import { getLanguages, getAccents } from '../actions/register';
 import makeAnimated from 'react-select/animated';
 import 'bootstrap/dist/css/bootstrap.css';
 
 const ageOptions = [
-  { value: 'child', label: 'Child' },
-  { value: 'teen', label: 'Teen' },
-  { value: 'adult', label: 'Adult' },
-  { value: 'senior', label: 'Senior' }
+    { value: 'child', label: 'Child' },
+    { value: 'teen', label: 'Teen' },
+    { value: 'adult', label: 'Adult' },
+    { value: 'senior', label: 'Senior' }
 ];
 
 const languageOptions = [
-  { value: 'english', label: 'English' },
-  { value: 'spanish', label: 'Spanish' },
-  { value: 'german', label: 'German' },
-  { value: 'japanese', label: 'Japanese' },
-  { value: 'chinese', label: 'Chinese' }
+    { value: 'english', label: 'English' },
+    { value: 'spanish', label: 'Spanish' },
+    { value: 'german', label: 'German' },
+    { value: 'japanese', label: 'Japanese' },
+    { value: 'chinese', label: 'Chinese' }
 ];
 
 const accentOptions = [
-  { value: 'american', label: 'American' },
-  { value: 'british', label: 'British' },
-  { value: 'central american', label: 'Central American' },
-  { value: 'european spanish', label: 'European Spanish' }
+    { value: 'american', label: 'American' },
+    { value: 'british', label: 'British' },
+    { value: 'central american', label: 'Central American' },
+    { value: 'european spanish', label: 'European Spanish' }
 ];
 
 class TalentProfile extends React.Component {
-  state = {
-    gender: '',
-    voiceAge: '',
-    languages: [],
-    accents: [],
-    biography: ''
-  };
+    constructor(props) {
+        super(props);
 
-  handleChange = event => {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-  };
-
-  handleAgeChange = voiceAge => {
-    this.setState({ voiceAge: voiceAge.value });
-  };
-
-  handleLanguageChange = languages => {
-    if (languages === null) {
-      languages = [];
-    } else {
-      this.setState({ languages });
+        this.state = {
+            gender: '',
+            voiceAge: '',
+            languageOptions: [],
+            accentOptions: [],
+            languages: [],
+            accents: [],
+            biography: ''
+        };
     }
-  };
 
-  destructureLanguages = languages => {
-    const newArray = languages.map(lang => lang.value);
-    console.log(newArray);
-    this.setState({ languages: newArray });
-  };
+    componentDidMount() {
+        this.props.getLanguages().then(this.modifyLanguage);
+        this.props.getAccents().then(this.modifyAccents);
+    }
 
-  handleAccentChange = accents => {
-    this.setState({ accents });
-  };
+    modifyLanguage = () => {
+        const newArray = this.props.languageOptions.map(item => ({
+            value: item.language,
+            label: item.language,
+            languageId: item.languageId
+        }));
 
-  handleSubmit = event => {
-    event.preventDefault();
-    this.destructureLanguages(this.state.languages);
-    console.log('Profile Saved');
-  };
+        this.setState({ languageOptions: newArray });
+    };
 
-  render() {
-    return (
-      <div className="TalentProfile">
-        <h1>Talent Profile</h1>
-        <Form onSubmit={this.handleSubmit}>
-          <FormGroup tag="fieldset">
-            <legend>Gender</legend>
-            <FormGroup check>
-              <Label check>
-                <Input
-                  type="radio"
-                  name="gender"
-                  value="male"
-                  onChange={this.handleChange}
-                />{' '}
-                Male
-              </Label>
-            </FormGroup>
-            <FormGroup check>
-              <Label check>
-                <Input
-                  type="radio"
-                  name="gender"
-                  value="female"
-                  onChange={this.handleChange}
-                />{' '}
-                Female
-              </Label>
-            </FormGroup>
-          </FormGroup>{' '}
-          <FormGroup>
-            <Label for="voiceAgeSelect">Select Voice Age</Label>
-            <Select
-              className="mt-4 col-md-8 col-offset-4"
-              onChange={this.handleAgeChange}
-              components={makeAnimated()}
-              options={ageOptions}
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="languageSelect">Select Languages</Label>
-            <Select
-              className="mt-4 col-md-8 col-offset-4"
-              onChange={this.handleLanguageChange}
-              components={makeAnimated()}
-              isMulti
-              options={languageOptions}
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="accentSelect">Select Accents</Label>
-            <Select
-              className="mt-4 col-md-8 col-offset-4"
-              onChange={this.handleAccentChange}
-              components={makeAnimated()}
-              isMulti
-              options={accentOptions}
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="bioText">Bio (Tell us a bit about yourself)</Label>
-            <Input
-              type="textarea"
-              name="biography"
-              id="bioText"
-              onChange={this.handleChange}
-            />
-          </FormGroup>
-          <Button>Save Profile</Button>
-        </Form>
-      </div>
-    );
-  }
+    modifyAccents = () => {
+        const newArray = this.props.accentOptions.map(item => ({
+            value: item.accent,
+            label: item.accent,
+            accentId: item.accentId
+        }));
+        this.setState({ accentOptions: newArray });
+    };
+
+    handleChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
+
+    handleAgeChange = voiceAge => {
+        this.setState({ voiceAge: voiceAge.value });
+    };
+
+    handleLanguageChange = languages => {
+        if (languages === null) {
+            languages = [];
+        } else {
+            this.setState({ languages });
+        }
+    };
+
+    destructureLanguages = languages => {
+        const newArray = languages.map(lang => lang.value);
+        console.log(newArray);
+        this.setState({ languages: newArray });
+    };
+
+    handleAccentChange = accents => {
+        this.setState({ accents });
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+        this.destructureLanguages(this.state.languages);
+        console.log('Profile Saved');
+    };
+
+    render() {
+        return (
+            <div className="TalentProfile">
+                <h1>Talent Profile</h1>
+                <Form onSubmit={this.handleSubmit}>
+                    <FormGroup tag="fieldset">
+                        <legend>Gender</legend>
+                        <FormGroup check>
+                            <Label check>
+                                <Input
+                                    type="radio"
+                                    name="gender"
+                                    value="male"
+                                    onChange={this.handleChange}
+                                />{' '}
+                                Male
+                            </Label>
+                        </FormGroup>
+                        <FormGroup check>
+                            <Label check>
+                                <Input
+                                    type="radio"
+                                    name="gender"
+                                    value="female"
+                                    onChange={this.handleChange}
+                                />{' '}
+                                Female
+                            </Label>
+                        </FormGroup>
+                    </FormGroup>{' '}
+                    <FormGroup>
+                        <Label for="voiceAgeSelect">Select Voice Age</Label>
+                        <Select
+                            className="mt-4 col-md-8 col-offset-4"
+                            onChange={this.handleAgeChange}
+                            components={makeAnimated()}
+                            options={ageOptions}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="languageSelect">Select Languages</Label>
+                        <Select
+                            className="mt-4 col-md-8 col-offset-4"
+                            onChange={this.handleLanguageChange}
+                            components={makeAnimated()}
+                            isMulti
+                            options={this.state.languageOptions}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="accentSelect">Select Accents</Label>
+                        <Select
+                            className="mt-4 col-md-8 col-offset-4"
+                            onChange={this.handleAccentChange}
+                            components={makeAnimated()}
+                            isMulti
+                            options={this.state.accentOptions}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="bioText">
+                            Bio (Tell us a bit about yourself)
+                        </Label>
+                        <Input
+                            type="textarea"
+                            name="biography"
+                            id="bioText"
+                            onChange={this.handleChange}
+                        />
+                    </FormGroup>
+                    <Button>Save Profile</Button>
+                </Form>
+            </div>
+        );
+    }
 }
 
-export default TalentProfile;
+const mapStateToProps = state => ({
+    languageOptions: state.registerReducer.languages,
+    accentOptions: state.registerReducer.accents
+});
+
+export default connect(mapStateToProps, { getAccents, getLanguages })(
+    TalentProfile
+);
+// export default TalentProfile;
