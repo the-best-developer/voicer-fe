5d5a85d8f16a1e91cf1b7e7fd3d6aa0120cd1c44
Jeremy Morales <jeremy.morales@gmail.com>
2019-11-19 21:08:14 -0800

    prettier format editing

--- voicer-client/src/actions/index.js
+++ voicer-client/src/actions/index.js
@@ -1,76 +1,76 @@
 export {
-  REGISTER_START,
-  REGISTER_SUCCESS,
-  REGISTER_FAILED,
-  CREATE_PROFILE_START,
-  CREATE_PROFILE_SUCCESS,
-  CREATE_PROFILE_FAILURE,
-  GET_LANGUAGES_START,
-  GET_LANGUAGES_SUCESS,
-  GET_LANGUAGES_FAILURE,
-  GET_ACCENTS_START,
-  GET_ACCENTS_SUCCESS,
-  GET_ACCENTS_FAIL,
-  ADD_TALENT_LANGUAGE_START,
-  ADD_TALENT_LANGUAGE_SUCCESS,
-  ADD_TALENT_LANGUAGE_FAIL,
-  ADD_TALENT_ACCENT_START,
-  ADD_TALENT_ACCENT_SUCCESS,
-  ADD_TALENT_ACCENT_FAIL,
-  createClientProfile,
-  createTalentProfile,
-  getLanguages,
-  getAccents,
-  register,
-  addTalentAccent,
-  addTalentLanguage
+    REGISTER_START,
+    REGISTER_SUCCESS,
+    REGISTER_FAILED,
+    CREATE_PROFILE_START,
+    CREATE_PROFILE_SUCCESS,
+    CREATE_PROFILE_FAILURE,
+    GET_LANGUAGES_START,
+    GET_LANGUAGES_SUCESS,
+    GET_LANGUAGES_FAILURE,
+    GET_ACCENTS_START,
+    GET_ACCENTS_SUCCESS,
+    GET_ACCENTS_FAIL,
+    ADD_TALENT_LANGUAGE_START,
+    ADD_TALENT_LANGUAGE_SUCCESS,
+    ADD_TALENT_LANGUAGE_FAIL,
+    ADD_TALENT_ACCENT_START,
+    ADD_TALENT_ACCENT_SUCCESS,
+    ADD_TALENT_ACCENT_FAIL,
+    createClientProfile,
+    createTalentProfile,
+    getLanguages,
+    getAccents,
+    register,
+    addTalentAccent,
+    addTalentLanguage
 } from './register';
 
 export { LOGIN_START, LOGIN_SUCCESS, LOGIN_FAILED, login } from './login';
 
 export {
-  EDIT_CLIENT_PROFILE_START,
-  EDIT_CLIENT_PROFILE_SUCCESS,
-  EDIT_CLIENT_PROFILE_FAILURE,
-  editClientProfile
+    EDIT_CLIENT_PROFILE_START,
+    EDIT_CLIENT_PROFILE_SUCCESS,
+    EDIT_CLIENT_PROFILE_FAILURE,
+    editClientProfile
 } from './editClientProfile';
 
 export {
-  GET_CLIENT_PROFILE_START,
-  GET_CLIENT_PROFILE_SUCCESS,
-  GET_CLIENT_PROFILE_FAILURE,
-  getClientProfile
+    GET_CLIENT_PROFILE_START,
+    GET_CLIENT_PROFILE_SUCCESS,
+    GET_CLIENT_PROFILE_FAILURE,
+    getClientProfile
 } from './getClientProfile';
 
 export {
-  POST_JOB_START,
-  POST_JOB_SUCCESS,
-  POST_JOB_FAILED,
-  postJob
+    POST_JOB_START,
+    POST_JOB_SUCCESS,
+    POST_JOB_FAILED,
+    postJob
 } from './postJob';
 
 export {
-  GET_JOBS_START,
-  GET_JOBS_SUCCESS,
-  GET_JOBS_FAILED,
-  getJobs,
-  getJobsBy
+    GET_JOBS_START,
+    GET_JOBS_SUCCESS,
+    GET_JOBS_FAILED,
+    getJobs,
+    getJobsBy
 } from './getJobs';
 
 export { APPLY_START, APPLY_SUCCESS, APPLY_FAILED, apply } from './apply';
 
 export {
-  GET_TALENT_START,
-  GET_TALENT_SUCCESS,
-  GET_TALENT_FAILED,
-  getTalent
+    GET_TALENT_START,
+    GET_TALENT_SUCCESS,
+    GET_TALENT_FAILED,
+    getTalent
 } from './getTalent';
 
 export {
-  GET_JOB_OFFERS_START,
-  GET_JOB_OFFERS_SUCCESS,
-  GET_JOB_OFFERS_FAILED,
-  getApplications
+    GET_JOB_OFFERS_START,
+    GET_JOB_OFFERS_SUCCESS,
+    GET_JOB_OFFERS_FAILED,
+    getApplications
 } from './getApplications';
 
 export { SET_JOB_ID, setJobId } from './setJobId';
