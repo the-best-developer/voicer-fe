6bb5bc2e1791f59b042cc84ece338ed83377e44d
Jeremy Morales <jeremy.morales@gmail.com>
2019-11-19 20:19:17 -0800

    prettier format update

--- voicer-client/src/components/TalentProfile.js
+++ voicer-client/src/components/TalentProfile.js
@@ -3,19 +3,19 @@ import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
 import Select, { components } from 'react-select';
 import { connect } from 'react-redux';
 import {
-  getLanguages,
-  getAccents,
-  addTalentLanguage,
-  addTalentAccent
+    getLanguages,
+    getAccents,
+    addTalentLanguage,
+    addTalentAccent
 } from '../actions/register';
 import makeAnimated from 'react-select/animated';
 import 'bootstrap/dist/css/bootstrap.css';
 
 const ageOptions = [
-  { value: 'child', label: 'Child' },
-  { value: 'teen', label: 'Teen' },
-  { value: 'adult', label: 'Adult' },
-  { value: 'senior', label: 'Senior' }
+    { value: 'child', label: 'Child' },
+    { value: 'teen', label: 'Teen' },
+    { value: 'adult', label: 'Adult' },
+    { value: 'senior', label: 'Senior' }
 ];
 
 // const languageOptions = [
@@ -34,192 +34,194 @@ const ageOptions = [
 // ];
 
 class TalentProfile extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      gender: '',
-      voiceAge: '',
-      languageOptions: [],
-      accentOptions: [],
-      languages: [],
-      accents: [],
-      biography: ''
-    };
-  }
-
-  componentDidMount() {
-    this.props.getLanguages().then(this.modifyLanguage);
-    this.props.getAccents().then(this.modifyAccents);
-  }
-
-  modifyLanguage = () => {
-    const newArray = this.props.languageOptions.map(item => ({
-      value: item.language,
-      label: item.language,
-      languageId: item.languageId
-    }));
-
-    this.setState({ languageOptions: newArray });
-  };
-
-  modifyAccents = () => {
-    const newArray = this.props.accentOptions.map(item => ({
-      value: item.accent,
-      label: item.accent,
-      accentId: item.accentId
-    }));
-    this.setState({ accentOptions: newArray });
-  };
-
-  submitTalentLanguages = talentLangArray => {
-    talentLangArray.forEach(newLang => {
-      const langSubmit = {
-        userId: this.props.userId,
-        languageId: newLang.languageId
-      };
-      this.props.addTalentLanguage(langSubmit);
-    });
-  };
-
-  submitTalentAccents = talentAccentArray => {
-    talentAccentArray.forEach(newAccent => {
-      const accentSubmit = {
-        userId: this.props.userId,
-        accentId: newAccent.accentId
-      };
-      this.props.addTalentAccent(accentSubmit);
-    });
-  };
-
-  handleChange = event => {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-  };
-
-  handleAgeChange = voiceAge => {
-    this.setState({ voiceAge: voiceAge.value });
-  };
-
-  handleLanguageAdd = languageId => {
-    const newLang = {
-      userId: this.props.userId,
-      languageId: languageId
-    };
-  };
-
-  handleLanguageChange = languages => {
-    if (languages === null) {
-      languages = [];
-    } else {
-      this.setState({ languages });
+        this.state = {
+            gender: '',
+            voiceAge: '',
+            languageOptions: [],
+            accentOptions: [],
+            languages: [],
+            accents: [],
+            biography: ''
+        };
     }
-  };
 
-  destructureLanguages = languages => {
-    const newArray = languages.map(lang => lang.value);
-    console.log(newArray);
-    this.setState({ languages: newArray });
-  };
+    componentDidMount() {
+        this.props.getLanguages().then(this.modifyLanguage);
+        this.props.getAccents().then(this.modifyAccents);
+    }
 
-  handleAccentChange = accents => {
-    this.setState({ accents });
-  };
+    modifyLanguage = () => {
+        const newArray = this.props.languageOptions.map(item => ({
+            value: item.language,
+            label: item.language,
+            languageId: item.languageId
+        }));
 
-  handleSubmit = event => {
-    event.preventDefault();
-    this.submitTalentLanguages(this.state.languages);
-    this.submitTalentAccents(this.state.accents);
-    // this.destructureLanguages(this.state.languages);
-    console.log('Profile Saved');
-  };
+        this.setState({ languageOptions: newArray });
+    };
 
-  render() {
-    return (
-      <div style={{marginTop: '21vh'}} className="TalentProfile">
-        <h1>Talent Profile</h1>
-        <Form onSubmit={this.handleSubmit}>
-          <FormGroup tag="fieldset">
-            <legend>Gender</legend>
-            <FormGroup check>
-              <Label check>
-                <Input
-                  type="radio"
-                  name="gender"
-                  value="male"
-                  onChange={this.handleChange}
-                />{' '}
-                Male
-              </Label>
-            </FormGroup>
-            <FormGroup check>
-              <Label check>
-                <Input
-                  type="radio"
-                  name="gender"
-                  value="female"
-                  onChange={this.handleChange}
-                />{' '}
-                Female
-              </Label>
-            </FormGroup>
-          </FormGroup>{' '}
-          <FormGroup>
-            <Label for="voiceAgeSelect">Select Voice Age</Label>
-            <Select
-              className="mt-4 col-md-8 col-offset-4"
-              onChange={this.handleAgeChange}
-              components={makeAnimated()}
-              options={ageOptions}
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="languageSelect">Select Languages</Label>
-            <Select
-              className="mt-4 col-md-8 col-offset-4"
-              onChange={this.handleLanguageChange}
-              components={makeAnimated()}
-              isMulti
-              options={this.state.languageOptions}
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="accentSelect">Select Accents</Label>
-            <Select
-              className="mt-4 col-md-8 col-offset-4"
-              onChange={this.handleAccentChange}
-              components={makeAnimated()}
-              isMulti
-              options={this.state.accentOptions}
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="bioText">Bio (Tell us a bit about yourself)</Label>
-            <Input
-              type="textarea"
-              name="biography"
-              id="bioText"
-              onChange={this.handleChange}
-            />
-          </FormGroup>
-          <Button>Save Profile</Button>
-        </Form>
-      </div>
-    );
-  }
+    modifyAccents = () => {
+        const newArray = this.props.accentOptions.map(item => ({
+            value: item.accent,
+            label: item.accent,
+            accentId: item.accentId
+        }));
+        this.setState({ accentOptions: newArray });
+    };
+
+    submitTalentLanguages = talentLangArray => {
+        talentLangArray.forEach(newLang => {
+            const langSubmit = {
+                userId: this.props.userId,
+                languageId: newLang.languageId
+            };
+            this.props.addTalentLanguage(langSubmit);
+        });
+    };
+
+    submitTalentAccents = talentAccentArray => {
+        talentAccentArray.forEach(newAccent => {
+            const accentSubmit = {
+                userId: this.props.userId,
+                accentId: newAccent.accentId
+            };
+            this.props.addTalentAccent(accentSubmit);
+        });
+    };
+
+    handleChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
+
+    handleAgeChange = voiceAge => {
+        this.setState({ voiceAge: voiceAge.value });
+    };
+
+    handleLanguageAdd = languageId => {
+        const newLang = {
+            userId: this.props.userId,
+            languageId: languageId
+        };
+    };
+
+    handleLanguageChange = languages => {
+        if (languages === null) {
+            languages = [];
+        } else {
+            this.setState({ languages });
+        }
+    };
+
+    destructureLanguages = languages => {
+        const newArray = languages.map(lang => lang.value);
+        console.log(newArray);
+        this.setState({ languages: newArray });
+    };
+
+    handleAccentChange = accents => {
+        this.setState({ accents });
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+        this.submitTalentLanguages(this.state.languages);
+        this.submitTalentAccents(this.state.accents);
+        // this.destructureLanguages(this.state.languages);
+        console.log('Profile Saved');
+    };
+
+    render() {
+        return (
+            <div style={{ marginTop: '21vh' }} className="TalentProfile">
+                <h1>Talent Profile</h1>
+                <Form onSubmit={this.handleSubmit}>
+                    <FormGroup tag="fieldset">
+                        <legend>Gender</legend>
+                        <FormGroup check>
+                            <Label check>
+                                <Input
+                                    type="radio"
+                                    name="gender"
+                                    value="male"
+                                    onChange={this.handleChange}
+                                />{' '}
+                                Male
+                            </Label>
+                        </FormGroup>
+                        <FormGroup check>
+                            <Label check>
+                                <Input
+                                    type="radio"
+                                    name="gender"
+                                    value="female"
+                                    onChange={this.handleChange}
+                                />{' '}
+                                Female
+                            </Label>
+                        </FormGroup>
+                    </FormGroup>{' '}
+                    <FormGroup>
+                        <Label for="voiceAgeSelect">Select Voice Age</Label>
+                        <Select
+                            className="mt-4 col-md-8 col-offset-4"
+                            onChange={this.handleAgeChange}
+                            components={makeAnimated()}
+                            options={ageOptions}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="languageSelect">Select Languages</Label>
+                        <Select
+                            className="mt-4 col-md-8 col-offset-4"
+                            onChange={this.handleLanguageChange}
+                            components={makeAnimated()}
+                            isMulti
+                            options={this.state.languageOptions}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="accentSelect">Select Accents</Label>
+                        <Select
+                            className="mt-4 col-md-8 col-offset-4"
+                            onChange={this.handleAccentChange}
+                            components={makeAnimated()}
+                            isMulti
+                            options={this.state.accentOptions}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="bioText">
+                            Bio (Tell us a bit about yourself)
+                        </Label>
+                        <Input
+                            type="textarea"
+                            name="biography"
+                            id="bioText"
+                            onChange={this.handleChange}
+                        />
+                    </FormGroup>
+                    <Button>Save Profile</Button>
+                </Form>
+            </div>
+        );
+    }
 }
 
 const mapStateToProps = state => ({
-  userId: state.loginReducer.id,
-  languageOptions: state.registerReducer.languages,
-  accentOptions: state.registerReducer.accents
+    userId: state.loginReducer.id,
+    languageOptions: state.registerReducer.languages,
+    accentOptions: state.registerReducer.accents
 });
 
 export default connect(mapStateToProps, {
-  getAccents,
-  getLanguages,
-  addTalentAccent,
-  addTalentLanguage
+    getAccents,
+    getLanguages,
+    addTalentAccent,
+    addTalentLanguage
 })(TalentProfile);
 // export default TalentProfile;
--- voicer-client/src/reducers/registerReducer.js
+++ voicer-client/src/reducers/registerReducer.js
@@ -1,113 +1,113 @@
 import {
-  REGISTER_START,
-  REGISTER_SUCCESS,
-  REGISTER_FAILED,
-  GET_LANGUAGES_START,
-  GET_LANGUAGES_SUCESS,
-  GET_LANGUAGES_FAILURE,
-  GET_ACCENTS_START,
-  GET_ACCENTS_SUCCESS,
-  GET_ACCENTS_FAIL,
-  ADD_TALENT_LANGUAGE_START,
-  ADD_TALENT_LANGUAGE_SUCCESS,
-  ADD_TALENT_LANGUAGE_FAIL,
-  ADD_TALENT_ACCENT_START,
-  ADD_TALENT_ACCENT_SUCCESS,
-  ADD_TALENT_ACCENT_FAIL
+    REGISTER_START,
+    REGISTER_SUCCESS,
+    REGISTER_FAILED,
+    GET_LANGUAGES_START,
+    GET_LANGUAGES_SUCESS,
+    GET_LANGUAGES_FAILURE,
+    GET_ACCENTS_START,
+    GET_ACCENTS_SUCCESS,
+    GET_ACCENTS_FAIL,
+    ADD_TALENT_LANGUAGE_START,
+    ADD_TALENT_LANGUAGE_SUCCESS,
+    ADD_TALENT_LANGUAGE_FAIL,
+    ADD_TALENT_ACCENT_START,
+    ADD_TALENT_ACCENT_SUCCESS,
+    ADD_TALENT_ACCENT_FAIL
 } from '../actions';
 
 const initialState = {
-  signingUp: false,
-  id: null,
-  languages: null,
-  accents: null,
-  error: null,
-  talentLanguages: null,
-  talentAccents: null
+    signingUp: false,
+    id: null,
+    languages: null,
+    accents: null,
+    error: null,
+    talentLanguages: null,
+    talentAccents: null
 };
 
 export const registerReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case REGISTER_START:
-      return {
-        ...state,
-        signingUp: true
-      };
-    case REGISTER_SUCCESS:
-      const [id] = action.payload;
-      return {
-        ...state,
-        id: id,
-        signingUp: false
-      };
-    case REGISTER_FAILED:
-      return {
-        ...state,
-        error: action.payload
-      };
-    case GET_LANGUAGES_START:
-      return {
-        ...state,
-        error: null
-      };
-    case GET_LANGUAGES_SUCESS:
-      return {
-        ...state,
-        languages: action.payload,
-        error: null
-      };
-    case GET_LANGUAGES_FAILURE:
-      return {
-        ...state,
-        error: action.payload
-      };
-    case GET_ACCENTS_START:
-      return {
-        ...state,
-        error: null
-      };
-    case GET_ACCENTS_SUCCESS:
-      return {
-        ...state,
-        accents: action.payload,
-        error: null
-      };
-    case GET_ACCENTS_FAIL:
-      return {
-        ...state,
-        accents: null,
-        error: null
-      };
-    case ADD_TALENT_LANGUAGE_START:
-      return {
-        ...state,
-        error: null
-      };
-    case ADD_TALENT_LANGUAGE_SUCCESS:
-      return {
-        ...state
-      };
-    case ADD_TALENT_LANGUAGE_FAIL:
-      return {
-        ...state,
-        error: action.payload
-      };
+    switch (action.type) {
+        case REGISTER_START:
+            return {
+                ...state,
+                signingUp: true
+            };
+        case REGISTER_SUCCESS:
+            const [id] = action.payload;
+            return {
+                ...state,
+                id: id,
+                signingUp: false
+            };
+        case REGISTER_FAILED:
+            return {
+                ...state,
+                error: action.payload
+            };
+        case GET_LANGUAGES_START:
+            return {
+                ...state,
+                error: null
+            };
+        case GET_LANGUAGES_SUCESS:
+            return {
+                ...state,
+                languages: action.payload,
+                error: null
+            };
+        case GET_LANGUAGES_FAILURE:
+            return {
+                ...state,
+                error: action.payload
+            };
+        case GET_ACCENTS_START:
+            return {
+                ...state,
+                error: null
+            };
+        case GET_ACCENTS_SUCCESS:
+            return {
+                ...state,
+                accents: action.payload,
+                error: null
+            };
+        case GET_ACCENTS_FAIL:
+            return {
+                ...state,
+                accents: null,
+                error: null
+            };
+        case ADD_TALENT_LANGUAGE_START:
+            return {
+                ...state,
+                error: null
+            };
+        case ADD_TALENT_LANGUAGE_SUCCESS:
+            return {
+                ...state
+            };
+        case ADD_TALENT_LANGUAGE_FAIL:
+            return {
+                ...state,
+                error: action.payload
+            };
 
-    case ADD_TALENT_ACCENT_START:
-      return {
-        ...state,
-        error: null
-      };
-    case ADD_TALENT_ACCENT_SUCCESS:
-      return {
-        ...state
-      };
-    case ADD_TALENT_ACCENT_FAIL:
-      return {
-        ...state,
-        error: action.payload
-      };
-    default:
-      return state;
-  }
+        case ADD_TALENT_ACCENT_START:
+            return {
+                ...state,
+                error: null
+            };
+        case ADD_TALENT_ACCENT_SUCCESS:
+            return {
+                ...state
+            };
+        case ADD_TALENT_ACCENT_FAIL:
+            return {
+                ...state,
+                error: action.payload
+            };
+        default:
+            return state;
+    }
 };
