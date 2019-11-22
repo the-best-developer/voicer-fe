1866eba456bcdb48a9348a4afce172e98f9c5b50
Ken Ruf <ruf.kenichi@gmail.com>
2019-11-21 21:26:03 -0600

    centered welcome message

--- voicer-client/src/components/ClientWelcome.js
+++ voicer-client/src/components/ClientWelcome.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ClientWelcome = () => {
     return (
-        <div>
+        <div className="job-list-container">
             <h3>Welcome to Voicer. Post a Job to get started.</h3>
         </div>
     )
