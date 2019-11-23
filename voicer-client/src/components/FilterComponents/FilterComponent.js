b8cca8556e808a45335750c36c76977564f3b69d
renegade27 <foemaster@live.com>
2019-11-23 11:58:13 -0600

    Fix styling merge issue for Client

--- voicer-client/src/components/FilterComponents/FilterComponent.js
+++ voicer-client/src/components/FilterComponents/FilterComponent.js
@@ -18,6 +18,7 @@ import { filterData } from '../../actions/filterData';
 const MainDiv = styled.div`
   min-width: 250px;
   max-width: 25%;
+  height: 80vh;
   min-height: 100%;
   display: flex;
   flex-direction: column;
