7e4a3368af62826ad52d41f8b374c7398d9a59fd
alphaseinor <alphaseinor@gmail.com>
2020-05-16 11:36:50 -0500

    added Marketplace and DropDown reducing all components of NavBar.jsx to under 25 lines for complexity reasons

--- voicer-client/src/components/navbar/NavBar.jsx
+++ voicer-client/src/components/navbar/NavBar.jsx
@@ -1,83 +1,60 @@
 import React, { useState, useContext } from "react"
 import LogRegFields from "./login/logRegFields"
-import { ReactComponent as Caret } from "../../images/caret.svg"
-import { Link } from "react-router-dom"
 
+import { ReactComponent as Caret } from "../../images/caret.svg"
 import { DataContext } from "../../context/DataContext"
 import Logo from "./Logo"
 import Logout from "./Logout"
 import MyProfile from "./MyProfile"
 import PostAJob from "./PostAJob"
+import AllUsers from "./AllUsers"
+import Marketplace from "./Marketplace"
 
 const NavBar = () => {
-  const { token } = useContext(DataContext)
   const [dropDown, setDropDown] = useState(false)
-  const [loginRegister, setLoginRegister] = useState(false)
-
-  const loginRegisterHandler = (e) => {
-    e.preventDefault()
-    setLoginRegister(!loginRegister)
-  }
+  
 
   return (
     <>
       <header>
         <nav className="navbar">
           <Logo />
-          {/* {
-            // if logged in, add a 'Post job' link to nav bar
-            token ? <p className="post-job-nav">Post a job</p> : <></>
-          } */}
-
           <ul className="navbar-nav">
-            <NavItem
-              icon={<Caret />}
-              dropDown={dropDown}
-              setDropDown={setDropDown}
-            >
-              <aside className="dropdown">
-                <div className="menu">
-                  <Link
-                    to={`/`}
-                    className="menu-item"
-                    onClick={() => {
-                      setDropDown(false)
-                    }}
-                  >
-                    Marketplace{" "}
-                  </Link>
-                  <Link
-                    to={`/voice`}
-                    className="menu-item"
-                    onClick={() => {
-                      setDropDown(false)
-                    }}
-                  >
-                    All Users
-                  </Link>
-                  <hr />
-                  {token ? (
-                    <>
-                      <MyProfile setDropDown={setDropDown}/>
-                      <PostAJob setDropDown={setDropDown}/>
-                      <Logout setDropDown={setDropDown} />
-                    </>
-                  ) : (
-                    <LogRegFields
-                      setDropDown={setDropDown}
-                      className="login-item"
-                      register={loginRegister}
-                      loginRegisterHandler={loginRegisterHandler}
-                      setLoginRegister={setLoginRegister}
-                    />
-                  )}
-                </div>
-              </aside>
+            <NavItem icon={<Caret />} dropDown={dropDown} setDropDown={setDropDown}>
+            <DropDown setDropDown={setDropDown} />
             </NavItem>
           </ul>
         </nav>
       </header>
     </>
+  )
+}
+
+const DropDown = ({setDropDown}) => {
+  const { token } = useContext(DataContext)
+  const [loginRegister, setLoginRegister] = useState(false)
+
+  const loginRegisterHandler = (e) => {
+    e.preventDefault()
+    setLoginRegister(!loginRegister)
+  }
+  return (
+    <aside className="dropdown">
+      <div className="menu">
+        <Marketplace setDropDown={setDropDown} />
+        <AllUsers setDropDown={setDropDown} />
+        <hr />
+        {token ? (
+          <>
+            <MyProfile setDropDown={setDropDown}/>
+            <PostAJob setDropDown={setDropDown}/>
+            <Logout setDropDown={setDropDown} />
+          </>
+        ) : (
+          <LogRegFields setDropDown={setDropDown} className="login-item" register={loginRegister} loginRegisterHandler={loginRegisterHandler} setLoginRegister={setLoginRegister} />
+        )}
+      </div>
+    </aside>
   )
 }
 
